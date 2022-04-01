e3decd31bd4e37738d96471962035435b03b593b
Mirko Moschella <shary.mirko@gmail.com>
2022-04-01 15:12:04 +0200

    creo la repo, carico le img, inzio l'header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderPhlox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderPhlox from "./components/HeaderPhlox.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderPhlox,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap");
+@import url("./assets/style/style.scss");
 </style>
---
+++ src/components/HeaderPhlox.vue
@@ -0,0 +1,70 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="head-side">
+        <div class="logo">
+          <img src="../../public/img/logotype.png" alt="logo" />
+        </div>
+        <div class="small-slogan">
+          <p>ALWATS THE BEST WAY YOU NEED IT</p>
+        </div>
+        <div class="large-slogan">
+          <h1>THE BEST BUSINESS CONSULTING</h1>
+        </div>
+      </div>
+      <div class="head-jumbo"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderPhlox.vue",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: aqua;
+  height: 480px;
+  display: flex;
+}
+.head-side {
+  width: 35%;
+  height: 680px;
+  background: url("../../public/img/7576.svg");
+  background-position: center;
+  .logo {
+    padding: 0.5rem 4rem;
+    img {
+      width: 90px;
+    }
+  }
+  .small-slogan {
+    padding: 10rem 0 0 13.5rem;
+    p {
+      color: white;
+      font-weight: 200;
+      font-size: 0.8rem;
+    }
+  }
+  .large-slogan {
+    width: 30%;
+    margin: 0 0 0 10rem;
+    position: absolute;
+    h1 {
+      color: white;
+      font-size: 3rem;
+      letter-spacing: 5px;
+      line-height: 45px;
+    }
+  }
+}
+.head-jumbo {
+  background-image: url("../../public/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 65%;
+  height: 680px;
+}
+</style>
